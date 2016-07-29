ed2bef36de691eefb06c4285d53d3652585caa7f
loveKino <842914439@qq.com>
2016-07-29 17:53:51 +0800

    init

---
+++ src/captureEvent.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * capture event
+ *
+ * opts = {
+ *      onlyUserAction: true
+ * }
+ *
+ * !!!use this script at the head of the page, so we can guarentee our event handler will run at the first time.
+ */
+
+module.exports = (eventList, callback, opts = {}) => {
+    // TODO window close event
+    let captureUIAction = (document) => {
+        // dom event
+        eventList.forEach((item) => {
+            document.addEventListener(item, (e) => {
+                if (opts.onlyUserAction) {
+                    if (e.isTrusted) {
+                        callback && callback(e);
+                    }
+                } else {
+                    callback && callback(e);
+                }
+            }, true); // capture model
+        });
+    };
+
+    captureUIAction(window.document);
+};
---
+++ src/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+let captureEvent = require('./captureEvent');
+
+let {
+    serializeEvent, serializeNode, serializePath
+} = require('serialize-front');
+
+let getAttachedUIStates = (node) => {
+    return {
+        window: {
+            pageYOffset: window.pageYOffset,
+            pageXOffset: window.pageXOffset
+        },
+
+        current: {
+            value: node.value,
+            scrollTop: node.scrollTop,
+            scrollLeft: node.scrollLeft
+        }
+    };
+};
+
+module.exports = (opts = {}) => {
+    opts.eventList = opts.eventList || [];
+
+    if (opts.onlyUserAction === undefined) {
+        opts.onlyUserAction = true;
+    }
+
+    if (opts.textContent === undefined) {
+        opts.textContent = true;
+    }
+
+    let getAction = (event) => {
+        let node = event.target;
+        event = serializeEvent(event);
+        let path = serializePath(node);
+
+        let nodeInfo = serializeNode(node, {
+            textContent: opts.textContent
+        });
+
+        return {
+            event: event,
+            time: new Date().getTime(),
+            attachedUIStates: getAttachedUIStates(node),
+            source: {
+                path,
+                node: nodeInfo
+            },
+            extra: {
+                url: window.location.href,
+                pageTitle: window.document.title
+            }
+        };
+    };
+
+    return {
+        capture: (handle) => {
+            captureEvent(opts.eventTypeList || [], event => {
+                let action = getAction(event);
+                handle(action, event);
+            }, {
+                onlyUserAction: opts.onlyUserAction
+            });
+        }
+    };
+};
